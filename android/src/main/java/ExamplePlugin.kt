bd5a1975a7257fcaabfb710cdb9b7a000f56e8fc
AverTry <72357571+AverTry@users.noreply.github.com>
2026-04-26 18:40:00 +0100

    instance

--- android/src/main/java/ExamplePlugin.kt
+++ android/src/main/java/ExamplePlugin.kt
@@ -89,7 +89,7 @@ class MusicNotificationPlugin(private val activity: Activity): Plugin(activity) 
         private const val PREFS_NAME = "music_notification"
         private const val PREF_SERVER_LIB_NAME = "server_lib_name"
 
-        // var instance: MusicNotificationPlugin? = null
+        var instance: MusicNotificationPlugin? = null
 
         fun getServerLibName(context: Context): String? {
             val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
